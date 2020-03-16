b27e9274143bc4880476a193887d2339fa2f0830
Kjerand Pedersen <kjerandp@lyse.net>
2020-03-16 20:28:51 +0100

    Added legacy support through CurveInterpolator2D class

---
+++ src/curve-interpolator-2d.ts
@@ -0,0 +1,104 @@
+import CurveInterpolator from './curve-interpolator';
+import { Vector, VectorType, BBox } from './interfaces';
+import { getTangentAtT } from './core';
+import { orthogonal, normalize } from './math';
+
+/**
+ * Bounding box interface
+ */
+export interface BBoxLegacy extends BBox {
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number,
+}
+
+/**
+ * Legacy support for v1 interface (2d curves)
+ */
+export default class CurveInterpolator2D extends CurveInterpolator {
+  constructor(points: Vector[], tension: number = 0.5, arcDivisions: number = 300, closed: boolean = false) {
+    super(points, { tension, arcDivisions, closed });
+  }
+
+  /**
+   * Find at which value(s) of x the curve is intersected by the given value
+   * along the y-axis
+   * @param y value at y-axis
+   * @param max max solutions (i.e. 0=all, 1=first along curve, -1=last along curve)
+   */
+  x(y: number, max: number = 0, margin: number = this._lmargin) : number | number[] {
+    const res = this.lookup(y, 1, max, margin);
+
+    if (Math.abs(max) === 1) {
+      return res[0] as number;
+    }
+    return (res as number[]).map(d => d[0]);
+  }
+
+  /**
+   * Find at which value(s) of y the curve is intersected by the given value
+   * along the x-axis
+   * @param x value at x-axis
+   * @param max max solutions (i.e. 0=all, 1=first along curve, -1=last along curve)
+   */
+  y(x: number, max: number = 0, margin: number = this._lmargin) : number | number[] {
+    const res = this.lookup(x, 0, max, margin);
+
+    if (Math.abs(max) === 1) {
+      return res[1] as number;
+    }
+    return (res as number[]).map(d => d[1]);
+  }
+
+  /**
+   * Get the normal at the given position.
+   * @param position position on curve (0 - 1)
+   * @param target optional target
+   */
+  getNormalAt<T extends VectorType>(position:number, target: T) : T
+  getNormalAt(position: number) : Vector
+  getNormalAt(position:number, target?:Vector) : Vector {
+    const tan = getTangentAtT(
+      this.getT(position),
+      this.points,
+      { tension: this.tension, closed: this.closed },
+      target,
+    );
+    const nrm = orthogonal(tan);
+    return normalize(nrm);
+  }
+
+  /**
+   * Get the angle (in radians) at the given position.
+   * @param position position on curve (0 - 1)
+   * @param target optional target
+   */
+  getAngleAt(position:number) : number {
+    const tan = getTangentAtT(
+      this.getT(position),
+      this.points,
+      { tension: this.tension, closed: this.closed },
+    );
+    return Math.atan2(tan[1], tan[0]);
+  }
+
+  /**
+   * Get a bounding box for the curve or the segment given by the
+   * from and to parameters
+   * @param from position from
+   * @param to position to
+   */
+  getBoundingBox(from:number = 0, to:number = 1) : BBoxLegacy {
+    const bbox = super.getBoundingBox(from, to);
+
+    return {
+      x1: bbox.min[0],
+      x2: bbox.max[0],
+      y1: bbox.min[1],
+      y2: bbox.max[1],
+      min: bbox.min,
+      max: bbox.max,
+    };
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as CurveInterpolator } from './curve-interpolator';
+export { default as CurveInterpolator2D } from './curve-interpolator-2d';
 export { default as Point } from './point';
 
 export * from './math';
